89fd66b166ba949f68ebe8ef2748827c9d0b4570
gamerjeannedielman <junes.dreyfus@gmail.com>
2024-10-01 20:13:33 -0400

    Final touches
    Finishing touches before I go to sleep.
    *Ghost now starts hidden between wall
    *added a tinier angrier ghost (where am i going with this)

--- you_got_ghosts_on_your_phone_2024_09_26_19_35_16/sketch.js
+++ you_got_ghosts_on_your_phone_2024_09_26_19_35_16/sketch.js
@@ -1,4 +1,5 @@
 
+//a kiyoshi kurosawa rip off/
 function setup() {
   createCanvas(600, 400);
   //removing cursor/
@@ -9,7 +10,7 @@ function setup() {
 }
 
 
-//defining how spooky is that screen/
+//defining screen/
  let screen={
    x:40,
    y:20,
@@ -36,10 +37,12 @@ function setup() {
  }
  
  let ghost={
-   x:screen.w/2,
-   y:screen.h/2,
+   x:3*screen.w/4,
+   y:1.25*screen.h/2,
    w:40,
-   h:100,
+   w2:0.5,
+   h:80,
+   h2:1,
    r:49,
    g:44,
    b:44,
@@ -57,24 +60,20 @@ function mousePressed() {
   screen.b+=0.3
   creepyroom.g+=0.3
   creepyroom.b+=0.1
-  
-  if (creepyroom.b=45.5)
-    {
-  ghost.x+=2.3
-  ghost.y+=1.8
+  ghost.x-=1.1
+  ghost.y-=0.2
   ghost.w+=1
   ghost.h+=1.5
   ghost.r-=0.4
   ghost.g-=0.4
   ghost.b-=0.4
-    }
+  ghost.w2*=1.1
+  ghost.h2*=1.1
+
 }
 
 //when mouse moved, webcam blinks on/
 //someone is watching//
-
-
-
 function mouseMoved() {
   webcam.fill += 5;
   if (webcam.fill>200) {
@@ -100,7 +99,7 @@ function mouseMoved() {
 //   }
 // }
 
-//might add third version with arrays//
+//might add third version with arrays if mathilde pulls through//
 
 // }
 
@@ -124,7 +123,7 @@ function draw() {
   let x2= constrain (mouseX, screen.x, screen.x+screen.w-25);
   let y2= constrain (mouseY+30, screen.y+30, screen.y+screen.h );
   
-  let x3= constrain (mouseX+25, screen.x+25, screen.w+40);
+  let x3= constrain (mouseX+25, screen.x+25, screen.w+screen.x);
   let y3= constrain (mouseY+20, screen.y+20, screen.y+screen.h-10 );
   
   
@@ -163,12 +162,20 @@ let g= map(mouseX, screen.x, screen.x+screen.w, 1, 5);
   rect(screen.x,screen.y,screen.w, screen.h/1.5)
   pop();
   
-  
-  
   push();
   fill(creepyroom.r+g, creepyroom.g+g, creepyroom.b);
   //cursed cursed cursed equations/
   quad (screen.x, screen.y, screen.w/3, screen.h/3, screen.w/3, screen.h/1.5, screen.x, screen.y+screen.h)
+  
+  //ghost will first hide behind wall geometry/
+  push();
+  fill(ghost.r, ghost.g, ghost.b),   
+  //creepy unreleased desk/
+  //rect(2.5*screen.w/4, screen.h/2, 150, ) 
+  ellipse(ghost.x, ghost.y, ghost.w, ghost.h),
+  ellipse(ghost.x, ghost.y, ghost.w2, ghost.h2),
+  pop();
+  
   quad (screen.x+screen.w, screen.y, 2*screen.w/3, screen.h/3, 2*screen.w/3, screen.h/1.5, screen.x+screen.w, screen.y+screen.h)
   pop();
   
@@ -177,6 +184,7 @@ let g= map(mouseX, screen.x, screen.x+screen.w, 1, 5);
   fill(webcam.fill);
   circle(webcam.x, webcam.y, webcam.d)
   pop();
+  
   
   
   //formats the ghastly message/
@@ -189,16 +197,15 @@ let g= map(mouseX, screen.x, screen.x+screen.w, 1, 5);
   // keeping it in the corner of the screen when enlarged/
   text('Do you want to meet a ghost?_', screen.w+screen.x-140, screen.h+screen.y-10);
   
-  //ghost/
-  push();
-  fill(ghost.r, ghost.g, ghost.b),
-  ellipse(ghost.x, ghost.y, ghost.w, ghost.h),
+  //second smaller ghost because I can't make the first one move exponentially/
+    push();
+  fill(ghost.r, ghost.g, ghost.b),   
+  ellipse(ghost.x, ghost.y, ghost.w2, ghost.h2),
+  pop();
   
   //the mouse/
   push();
   fill('creepyroom.r ,creepyroom.g,creepyroom.b)');
   triangle(x1,y1, x2,y2,x3,y3);
   pop(); 
-  
-  
 }
